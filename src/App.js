2ca5d4e8b724954bf29ad74696e4b7b15d8ff8c6
usamabinnadeem-10 <32700508+usamabinnadeem-10@users.noreply.github.com>
2020-12-28 18:29:57 +0500

    Map Search added

--- src/App.js
+++ src/App.js
@@ -5,12 +5,59 @@ import Map from './components/map/Map'
 import Login from './components/auth/Login'
 import SignUp from './components/auth/SignUp'
 import Home from './components/home/Home'
+import MapSearch from './components/mapSearch/MapSearch'
+
+const dummyAds = [
+  {
+    "id": 1,
+    "name": "Park Slope",
+    "price" : 90000,
+    "img" : [
+      "https://specials-images.forbesimg.com/imageserve/1026205392/960x0.jpg?fit=scale",
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/modern-house-2-1538579843.jpg?crop=1.00xw:0.731xh;0,0.264xh&resize=980:*",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Tiny_house%2C_Portland.jpg/800px-Tiny_house%2C_Portland.jpg"
+    ],
+    "latitude": "40.6710729",
+    "longitude": "-73.9988001",
+  },
+  {
+    "id": 2,
+    "name": "Bushwick",
+    "price" : 40000,
+    "img" : [
+      "https://specials-images.forbesimg.com/imageserve/1026205392/960x0.jpg?fit=scale",
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/modern-house-2-1538579843.jpg?crop=1.00xw:0.731xh;0,0.264xh&resize=980:*"
+    ],
+    "latitude": "40.6942861",
+    "longitude": "-73.9389312",
+  },
+  {
+    "id": 3,
+    "name": "East New York",
+    "price" : 70000,
+    "img" : [
+      "https://specials-images.forbesimg.com/imageserve/1026205392/960x0.jpg?fit=scale",
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/modern-house-2-1538579843.jpg?crop=1.00xw:0.731xh;0,0.264xh&resize=980:*"
+    ],
+    "latitude": "40.6577799",
+    "longitude": "-73.9147716",
+  }
+]
+
 function App() {
 
   return (
     <div className="container-fluid">
       <Header/>
       <Home/>
+      <div className="mb-4 d-flex flex-column" style={{marginTop : '120px'}}>
+        <h2 className="col-10 mx-auto my-4">Map Search</h2>
+        <MapSearch
+          center={{ lat: 40.6451594, lng: -74.0850826 }}
+          zoom={10}
+          places={dummyAds}
+        />
+      </div>
       {/* <Ad/> */}
       {/* <Login/> */}
       {/* <SignUp/> */}
---
+++ src/components/mapSearch/MapSearch.js
@@ -0,0 +1,110 @@
+import React from "react";
+
+
+import {
+  withGoogleMap,
+  GoogleMap,
+  withScriptjs,
+  Marker,
+  InfoWindow
+} from "react-google-maps";
+
+
+import { compose, withProps, withStateHandlers } from "recompose";
+
+
+const MapSearch = compose(
+  withProps({
+    googleMapURL:
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyDYuHFLdk-1Yw9IfmS2dtogQj1vL4DkO2E&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div className="col-10 mx-auto" style={{ height: "70vh" }} />,
+    mapElement: <div style={{ height: "100%" }} />
+  }),
+  withStateHandlers(
+    props => ({
+      infoWindows: props.places.map(p => {
+        return { isOpen: false };
+      })
+    }),
+    {
+      onToggleOpen: ({ infoWindows }) => selectedIndex => ({
+        infoWindows: infoWindows.map((iw, i) => {
+          iw.isOpen = selectedIndex === i;
+          return iw;
+        })
+      })
+    }
+  ),
+  withScriptjs,
+  withGoogleMap
+)(props => (
+  <GoogleMap defaultZoom={props.zoom} defaultCenter={props.center}>
+    {props.places &&
+      props.places.map((place, i) => {
+        let lat = parseFloat(place.latitude, 10);
+        let lng = parseFloat(place.longitude, 10);
+
+        return (
+          <Marker
+            id={place.id}
+            key={place.id}
+            position={{ lat: lat, lng: lng }}
+            title="Click to zoom"
+            onClick={props.onToggleOpen.bind(this, i)}
+          >
+            {props.infoWindows[i].isOpen && (
+              <InfoWindow onCloseClick={props.onToggleOpen.bind(i)}>
+                <div className="p-3 d-flex flex-column">
+                    <h4>{place.name}</h4>
+                    <h5>{place.price}</h5>
+                    <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
+                    <ol className="carousel-indicators">
+                    {
+                        place.img.map((img,index)=>{
+                            return(
+                                index == 0 ? <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to={index} className="active"></li> : 
+                                <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to={index}></li>
+                                
+                            )
+                        })
+                    }
+                    </ol>
+                        <div className="carousel-inner">
+                            {
+                                place.img.map((img,index)=>{
+                                    return(
+                                        index == 0 ? (
+                                            <div className="carousel-item active">
+                                                <img src={img} className="d-block w-100"/>
+                                            </div>
+                                        ) : (
+                                            <div className="carousel-item">
+                                                <img src={img} className="d-block w-100"/>
+                                            </div>
+                                        )
+
+                                    )
+                                })
+                            }
+                            
+                        </div>
+                        <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-bs-slide="prev">
+                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span className="visually-hidden">Previous</span>
+                        </a>
+                        <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-bs-slide="next">
+                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span className="visually-hidden">Next</span>
+                        </a>
+                        </div>
+                </div>
+              </InfoWindow>
+            )}
+          </Marker>
+        );
+      })}
+  </GoogleMap>
+));
+
+export default MapSearch;
